75110b1da16daee99817fe0d178d268a5f1a4a3e
AlvaroCascoV <alvarocascovalero@gmail.com>
2025-10-27 10:47:07 +0100

    metodos globales con filters

--- src/components/MetodosFilters.vue
+++ src/components/MetodosFilters.vue
@@ -1,8 +1,24 @@
 <template>
 	<h1>Metodos Filters</h1>
+	<ul>
+		<li v-for="num in numeros" :key="num">
+			Número: {{ num }}, Doble: {{ $filters.getNumeroDoble(num) }}
+		</li>
+	</ul>
+	<ul>
+		<li v-for="name in nombres" :key="name">
+			{{ $filters.mayuscula(name) }}
+		</li>
+	</ul>
 </template>
 <script>
 	export default {
 		name: "MetodosFilters",
+		data() {
+			return {
+				numeros: [55, 21, 33, 44, 88],
+				nombres: ["Lucia", "Diana", "Antonia", "Carlos"],
+			};
+		},
 	};
 </script>
